d6f9cc48c56b9b8231c5a54429e245567ee87829
ItsLucas <itslucas@itslucas.me>
2021-04-14 17:26:26 +0800

    add htmls

--- src/main/java/me/itslucas/bookstore/controller/SearchController.java
+++ src/main/java/me/itslucas/bookstore/controller/SearchController.java
@@ -23,54 +23,5 @@ public class SearchController {
     @Autowired
     private BookService bookService;
 
-    @RequestMapping("/searchByCategory")
-    public String searchByCategory(
-            @RequestParam("category") String category,
-            Model model, Principal principal
-    ) {
-        if(principal != null) {
-            String username = principal.getName();
-            User user = userService.findByUsername(username);
-            model.addAttribute("user", user);
-        }
 
-        String classActiveCategory = "active"+category;
-        classActiveCategory = classActiveCategory.replaceAll("\\s","");
-        classActiveCategory = classActiveCategory.replaceAll("&","");
-        model.addAttribute("classActiveCategory", true);
-
-        List<Book> bookList = bookService.findByCategory(category);
-
-        if(bookList.isEmpty()) {
-            model.addAttribute("emptyList", true);
-            return "bookshelf";
-        }
-
-        model.addAttribute("bookList", bookList);
-
-        return "bookshelf";
-    }
-
-    @RequestMapping("/searchBook")
-    public String searchbook(
-            @ModelAttribute("keyword") String keyword,
-            Principal principal, Model model
-    ) {
-        if(principal != null) {
-            String username = principal.getName();
-            User user = userService.findByUsername(username);
-            model.addAttribute("user", user);
-        }
-
-        List<Book> bookList = bookService.blurrySearch(keyword);
-
-        if(bookList.isEmpty()) {
-            model.addAttribute("emptyList", true);
-            return "bookshelf";
-        }
-
-        model.addAttribute("bookList", bookList);
-
-        return "bookshelf";
-    }
 }
