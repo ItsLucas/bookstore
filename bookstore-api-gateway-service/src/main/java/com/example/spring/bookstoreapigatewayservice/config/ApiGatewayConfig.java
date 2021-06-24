87ff274278f90fbe6fc937c955c1f95c45351ff4
Chestnut <chestnutprog@outlook.com>
2021-06-24 20:41:31 +0800

    Add api gateway

---
+++ bookstore-api-gateway-service/src/main/java/com/example/spring/bookstoreapigatewayservice/config/ApiGatewayConfig.java
@@ -0,0 +1,33 @@
+package com.example.spring.bookstoreapigatewayservice.config;
+
+import brave.sampler.Sampler;
+import com.example.spring.bookstoreapigatewayservice.filters.PostFilter;
+import com.example.spring.bookstoreapigatewayservice.filters.PreFilter;
+import com.example.spring.bookstoreapigatewayservice.filters.RouteFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfig {
+
+    @Bean
+    public Sampler sampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+
+    @Bean
+    public PreFilter preFilter() {
+        return new PreFilter();
+    }
+
+    @Bean
+    public PostFilter postFilter() {
+        return new PostFilter();
+    }
+
+    @Bean
+    public RouteFilter routeFilter() {
+        return new RouteFilter();
+    }
+
+}
